2e25fbc5e958e1a1addc5b14b9e7a6b2f75f9da9
mwweinberg <mwweinberg@users.noreply.github.com>
2016-08-30 17:30:46 -0400

    added line breaks

--- china-daily-email-working.py
+++ china-daily-email-working.py
@@ -5,6 +5,8 @@ import csv
 
 #this will hold the output
 holder = {}
+#this will hold the unmatched URLs output
+unmatched_holder = []
 
 
 
@@ -60,22 +62,8 @@ def headliner(url):
             holder[headline_text] = article_text
 
 
-            """
-            output_txt.write(str(headline_text))
-            output_txt.write("\n")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            output_txt.write(str(headline_text))
-            output_txt.write("\n")
-            output_txt.write(str(article_text))
-            output_txt.write("\n")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            """
 
-        if "qz" in row_contents:
+        elif "qz" in row_contents:
             #opens the url for read access
             this_url = urllib.urlopen(row_contents).read()
             #creates a new BS holder based on the URL
@@ -106,22 +94,9 @@ def headliner(url):
 
             holder[headline_text] = article_text
 
-            """
-            output_txt.write(str(headline_text))
-            output_txt.write("\n")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            output_txt.write(str(headline_text))
-            output_txt.write("\n")
-            output_txt.write(str(article_text))
-            output_txt.write("\n")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            output_txt.write("\r")
-            """
 
-        if "foreignpolicy" in row_contents:
+
+        elif "foreignpolicy" in row_contents:
             #opens the url for read access
             this_url = urllib.urlopen(row_contents).read()
             #creates a new BS holder based on the URL
@@ -150,18 +125,382 @@ def headliner(url):
 
             holder[headline_text] = article_text
 
+        elif "sixthtone" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'Sixth Tone: '
+            headline = soup.find_all('h3', {"class":"heading-1"})
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"class" : "content"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "washingtonpost" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'Washington Post: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"id" : "article-body"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "latimes" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'LA Times: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"class" : "trb_ar_page"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "wsj" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'WSJ: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"id" : "wsj-article-wrap"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "chinadigitaltimes" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'China Digital Times: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"class" : "entry"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "tchrd" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'TCHRD: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"class" : "entry"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "scmp" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'SCMP: '
+            headline = soup.find_all('h1', {"itemprop": "name headline"}, {"class": "title", "id":"page-title"})
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+            article = soup.find_all('p')
+
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "caixin" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'Caixin: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            #this line adds the URL to the output text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"id" : "txt_content", 'style' : 'font-size:14px;'}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "reuters" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'Reuters: '
+            headline = soup.find_all('h1')
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            #this line adds the URL to the output text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("span", {"id" : "article-text"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "xinhuanet" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'Xinhua: '
+            headline = soup.find_all('span', {'id':'bltitle'})
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            #this line adds the URL to the output text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("span", {"id" : "content", "class":"hei14 pcCon"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "theguardian" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'The Guardian: '
+            headline = soup.find_all('h1', {"class":"content__headline js-score", "itemprop":"headline"})
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            #this line adds the URL to the output text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+
+            article = soup.find("div", {"class" : "content__article-body from-content-api js-article__body", "itemprop":"articleBody", "data-test-id":"article-review-body"}).findAll('p')
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
+        elif "scmp" in row_contents:
+            #opens the url for read access
+            this_url = urllib.urlopen(row_contents).read()
+            #creates a new BS holder based on the URL
+            soup = BeautifulSoup(this_url, 'lxml')
+
+            #creates the headline section
+            headline_text = 'SCMP: '
+            headline = soup.find_all('h1', {"itemprop": "name headline"}, {"class": "title", "id":"page-title"})
+            for element in headline:
+                    headline_text += ''.join(element.findAll(text = True)).encode('utf-8').strip()
+                    headline_text += "\n"
+
+
+
+
+            #creats the body text
+            #This turns the html text into regular text
+            article_text = row_contents + "\n" + "\r"
+            #This finds each paragraph
+            article = soup.find_all('p')
+
+            #for each paragraph
+            for element in article:
+                #add a line break and then the text part of the paragraph
+                #the .encode part fixes unicode bullshit
+                article_text += '\n' + ''.join(element.findAll(text = True)).encode('utf-8').strip()
+
+            holder[headline_text] = article_text
+
         else:
             print "not a story from a known source"
+            unmatched_holder.append(row_contents)
+
 
 headliner(txt)
 
 #this is just for debugging
-print holder
+#print holder
+#print "unmatched holder is %s" % (unmatched_holder)
+
+#iterates through the unmatched urls in unmatched_holder and writes them to the doc
+for item in unmatched_holder:
+    output_txt.write("cannot process %s" %(str(item)))
+    output_txt.write("\n")
+    output_txt.write("\r")
+    output_txt.write("\r")
 
 #iterates through the headlines in holder and writes them to the doc
 #this is the TOC
 for head, body in holder.items():
     output_txt.write(str(head))
+    output_txt.write("\n")
     output_txt.write("\r")
     output_txt.write("\r")
 
@@ -170,10 +509,13 @@ for head, body in holder.items():
 
 for head, body in holder.items():
     output_txt.write("\r")
+    output_txt.write("\n")
     output_txt.write(str(head))
+    output_txt.write("\n")
     output_txt.write("\r")
     output_txt.write("\r")
     output_txt.write(str(body))
+    output_txt.write("\n")
     output_txt.write("\r")
 
 
